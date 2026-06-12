fff88081d89f8c6f43012e9a9e7e9b0f7b3eca61
1004koreans <1004koreans@gmail.com>
2026-06-12 09:27:24 +0900

    6/12

--- 15lambda3.py
+++ 15lambda3.py
@@ -2,7 +2,7 @@ multilambda = lambda x: x*2
 listdata1=[1,2,3,4,-1,-2, -32,-4, -5, -10]
 result = list(map(multilambda, listdata1))
 print( 'final', result)
-
+#sss
 listdata2=[1,2,3,4,5,6,7,8,9,10]
 strlambda =lambda: '3X' if x%3==0 else x
 result = list (map(strlambda, listdata2))
